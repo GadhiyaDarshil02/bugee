98ef09c158e8536e0c37dc74c81ea606d5c428c6
GadhiyaDarshil02 <42084293+GadhiyaDarshil02@users.noreply.github.com>
2022-01-22 15:34:03 -0500

    Add files via upload

---
+++ CMS/academics.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Academics</title>
+</head>
+<body>
+	<?php include('common/header.php') ?>
+	<div class="container-fluid">
+		<div class="row mb-5">
+			<div class="academic-area-headings">
+				<h4 class="py-4">Faculty of Engineering and Technology</h4>
+			</div>
+			<div class="container">
+				<div class="row">
+					<h4 class="dean-message-heading mt-3">Dean's Message</h4>
+					<div class="col-lg-12 text-justify">
+						<p>
+							Lorem ipsum dolor sit amet. Et accusamus vero et quae dolorum quo cumque laborum At repellendus distinctio et error omnis eos distinctio temporibus. Placeat dolor aut voluptatem delectus vel nihil nisi. Ut voluptas dolorum aut iste galisum vel autem commodi et optio illo aut quia dicta.
+						</p>
+						<p>
+							A repudiandae error aut doloremque consectetur et culpa consequatur est galisum culpa eum adipisci tempore et excepturi facilis aut dolorem ullam! Est eveniet reiciendis et sequi consequatur aut voluptatem molestiae cum dolor repellat sed earum eaque id voluptate dolore aut distinctio reiciendis. Aut debitis aliquid sit quae voluptas ut quam eveniet qui quos unde. Eos reiciendis quaerat 33 tempore dolor et recusandae eius.
+
+						</p>
+						<p>
+							Ut assumenda voluptatum qui deleniti neque sed delectus voluptas et recusandae dolores vel culpa officia ab illum veniam? Est cupiditate consequatur qui impedit ipsa et asperiores voluptate qui dolores blanditiis. Qui adipisci culpa et dolor culpa est eligendi odit et soluta enim aut atque quia. Aut repudiandae perferendis ut doloribus dolorem ex ratione omnis in vero nulla cum atque architecto sed laborum dolor sit enim blanditiis
+						</p>
+					</div>	
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container-fluid">
+		<div class="row mb-5">
+			<div class="academic-area-headings">
+				<h4 class="py-4">Faculty of Art, Social Sciences and Humanities</h4>
+			</div>
+			<div class="container">
+				<div class="row">
+					<h4 class="dean-message-heading mt-3">Dean's Message</h4>
+					<div class="col-lg-12 text-justify">
+						<p>
+							Lorem ipsum dolor sit amet. Et accusamus vero et quae dolorum quo cumque laborum At repellendus distinctio et error omnis eos distinctio temporibus. Placeat dolor aut voluptatem delectus vel nihil nisi. Ut voluptas dolorum aut iste galisum vel autem commodi et optio illo aut quia dicta.
+						</p>
+						<p>
+							A repudiandae error aut doloremque consectetur et culpa consequatur est galisum culpa eum adipisci tempore et excepturi facilis aut dolorem ullam! Est eveniet reiciendis et sequi consequatur aut voluptatem molestiae cum dolor repellat sed earum eaque id voluptate dolore aut distinctio reiciendis. Aut debitis aliquid sit quae voluptas ut quam eveniet qui quos unde. Eos reiciendis quaerat 33 tempore dolor et recusandae eius.
+
+							
+						</p>
+						<p>
+							Ut assumenda voluptatum qui deleniti neque sed delectus voluptas et recusandae dolores vel culpa officia ab illum veniam? Est cupiditate consequatur qui impedit ipsa et asperiores voluptate qui dolores blanditiis. Qui adipisci culpa et dolor culpa est eligendi odit et soluta enim aut atque quia. Aut repudiandae perferendis ut doloribus dolorem ex ratione omnis in vero nulla cum atque architecto sed laborum dolor sit enim blanditiis
+						</p>
+					</div>	
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container-fluid">
+		<div class="row mb-5">
+			<div class="academic-area-headings">
+				<h4 class="py-4">Faulty Computer Sciences</h4>
+			</div>
+			<div class="container">
+				<div class="row">
+					<h4 class="dean-message-heading mt-3">Dean's Message</h4>
+					<div class="col-lg-12 text-justify">
+						<p>
+							Lorem ipsum dolor sit amet. Et accusamus vero et quae dolorum quo cumque laborum At repellendus distinctio et error omnis eos distinctio temporibus. Placeat dolor aut voluptatem delectus vel nihil nisi. Ut voluptas dolorum aut iste galisum vel autem commodi et optio illo aut quia dicta.
+						</p>
+						<p>
+							A repudiandae error aut doloremque consectetur et culpa consequatur est galisum culpa eum adipisci tempore et excepturi facilis aut dolorem ullam! Est eveniet reiciendis et sequi consequatur aut voluptatem molestiae cum dolor repellat sed earum eaque id voluptate dolore aut distinctio reiciendis. Aut debitis aliquid sit quae voluptas ut quam eveniet qui quos unde. Eos reiciendis quaerat 33 tempore dolor et recusandae eius.ng education henceforth would be to improve the soft skills and conﬁdence level of graduate and to encourage them to emerge as entrepreneur’s able to generate wealth.
+						</p>
+						<p>
+							Ut assumenda voluptatum qui deleniti neque sed delectus voluptas et recusandae dolores vel culpa officia ab illum veniam? Est cupiditate consequatur qui impedit ipsa et asperiores voluptate qui dolores blanditiis. Qui adipisci culpa et dolor culpa est eligendi odit et soluta enim aut atque quia. Aut repudiandae perferendis ut doloribus dolorem ex ratione omnis in vero nulla cum atque architecto sed laborum dolor sit enim blanditiis
+						</p>
+					</div>	
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container-fluid">
+		<div class="row mb-5">
+			<div class="academic-area-headings">
+				<h4 class="py-4">Faculty of Business Administration</h4>
+			</div>
+			<div class="container">
+				<div class="row">
+					<h4 class="dean-message-heading mt-3">Dean's Message</h4>
+					<div class="col-lg-12 text-justify">
+						<p>
+							Lorem ipsum dolor sit amet. Et accusamus vero et quae dolorum quo cumque laborum At repellendus distinctio et error omnis eos distinctio temporibus. Placeat dolor aut voluptatem delectus vel nihil nisi. Ut voluptas dolorum aut iste galisum vel autem commodi et optio illo aut quia dicta.
+						</p>
+						<p>
+							A repudiandae error aut doloremque consectetur et culpa consequatur est galisum culpa eum adipisci tempore et excepturi facilis aut dolorem ullam! Est eveniet reiciendis et sequi consequatur aut voluptatem molestiae cum dolor repellat sed earum eaque id voluptate dolore aut distinctio reiciendis. Aut debitis aliquid sit quae voluptas ut quam eveniet qui quos unde. Eos reiciendis quaerat 33 tempore dolor et recusandae eius.
+						</p>
+						<p>
+							Ut assumenda voluptatum qui deleniti neque sed delectus voluptas et recusandae dolores vel culpa officia ab illum veniam? Est cupiditate consequatur qui impedit ipsa et asperiores voluptate qui dolores blanditiis. Qui adipisci culpa et dolor culpa est eligendi odit et soluta enim aut atque quia. Aut repudiandae perferendis ut doloribus dolorem ex ratione omnis in vero nulla cum atque architecto sed laborum dolor sit enim blanditiis
+						</p>
+					</div>	
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
